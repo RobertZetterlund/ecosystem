909c95bfa1c79d6f957a253b1fb0f256166a0cba
Alex Solberg <alex.solberg@kurvard.se>
2020-02-17 17:28:26 +0100

    Implemented animal. The animal will get hungrier and thristier and then die at some point

---
+++ Assets/Animal.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    double hunger;
+    double thirst;
+    double timeToDeathByHunger = 200;
+    double timeToDeathByThirst = 200;
+    bool isDead;
+    double energy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // use Köres senses to do tings. innit bruv
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //increases hunger and thirst over time
+        hunger += Time.deltaTime * 1 / timeToDeathByHunger;
+        thirst += Time.deltaTime * 1 / timeToDeathByThirst;
+
+        isDead();
+
+    }
+
+    public void isDead(double hunger, double thirst) 
+    {
+        if (hunger >= 1) 
+        {
+            Die (CauseOfDeath.Hunger);
+        } 
+        else if (thirst >= 1) 
+        {
+            Die (CauseOfDeath.Thirst);
+        }
+        else if (energy <= 0) 
+        {
+            Die(CauseOfDeath.Age);
+        }
+    }
+
+     public booean Die (CauseOfDeath cause)
+     {
+        if (!isDead) {
+            isDead = true;
+            //Something.log(cause);
+            //Environment.RegisterDeath (this);
+            //Destroy (gameObject);
+        }
+    }
+}
