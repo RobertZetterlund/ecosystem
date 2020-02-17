edebdc91629caa800b7884c4443b7b0990c25a80
isaksc2 <isaksc@student.chalmers.se>
2020-02-17 16:52:51 +0100

    created water pond object
    consume method can be used for plant maybe

---
+++ Assets/WaterPond.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPond : MonoBehaviour
+
+{
+    double amount = 1;
+    double capacity = 1;
+
+    double consume(double amount)
+    {
+        if (this.amount - amount < 0) // wants to drink more than what is available
+        {
+            double consumed = this.amount;
+            this.amount = 0;
+            return consumed;
+        } else // can consome the desired amount
+        {
+            this.amount -= amount;
+            return amount;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
