ec8885b68619c3e05eb5d42a4ecf73724e3f6630
isaksc2 <isaksc@student.chalmers.se>
2020-02-17 17:14:26 +0100

    consumer interfaces + gameController

---
+++ Assets/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class GameController
+{
+	public GameController()
+	{
+	}
+
+    private void consume(IConsumer consumer, IConsumable consumable)
+    {
+
+    }
+
+}
---
+++ Assets/IConsumer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public interface IConsumer
+{
+	public IConsumer()
+	{
+	}
+}
