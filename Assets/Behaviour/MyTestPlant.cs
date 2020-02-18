91dd64e89769abe48316fd9d1a8b5ed38eac11c2
Alex Solberg <alex.solberg@kurvard.se>
2020-02-18 15:06:51 +0100

    added some functionality to the plant

--- Assets/Behaviour/MyTestPlant.cs
+++ Assets/Behaviour/MyTestPlant.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MyTestPlant : MonoBehaviour
 {
 
-
+    float amountRemaining = 1;
+    const float consumeSpeed = 8;
 
 
     // Start is called before the first frame update
@@ -18,5 +20,24 @@ public class MyTestPlant : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public float Consume (float amount) {
+        float amountConsumed = Mathf.Max (0, Mathf.Min (amountRemaining, amount));
+        amountRemaining -= amount * consumeSpeed;
+
+        transform.localScale = Vector3.one * amountRemaining;
+
+        if (amountRemaining <= 0) {
+            //Die (CauseOfDeath.Eaten);
+        }
+
+        return amountConsumed;
+    }
+
+    public float AmountRemaining {
+        get {
+            return amountRemaining;
+        }
     }
 }
