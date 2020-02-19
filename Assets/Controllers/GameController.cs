3a55f3a7e509943a23f60791ba1dcf9e89cc3232
isaksc2 <isaksc@student.chalmers.se>
2020-02-19 17:20:35 +0100

    implemented basic crossover and mutation

--- Assets/Controllers/GameController.cs
+++ Assets/Controllers/GameController.cs
@@ -2,7 +2,9 @@
 
 public class GameController
 {
-    private static double BITE_FACTOR = 0.1;
+    private static double BITE_FACTOR = 0.2;
+    private static double STD_DEVIATION_FACTOR = 0.2;
+    private static double MUTATION_CHANCE = 0.2;
 
 	public GameController()
 	{
@@ -24,6 +26,63 @@ public class GameController
             return (biteSize, type);
         }
     }
+
+    // Crossover and mutate
+    private static double ReproduceDouble(double geneA, double geneB, bool allowNegative)
+    {
+        double crossedGene = Crossover(geneA, geneB);
+
+        Random random = new Random();
+        if (random.NextDouble() < MUTATION_CHANCE)
+        {
+            return MutateDouble(crossedGene, allowNegative);
+        }
+        return crossedGene;
+    }
+
+    // Crossover and mutate
+    private static bool ReproduceBool(bool geneA, bool geneB, bool allowNegative)
+    {
+        bool crossedGene = Crossover(geneA, geneB);
+
+        Random random = new Random();
+        if (random.NextDouble() < MUTATION_CHANCE)
+        {
+            return MutateBool(crossedGene);
+        }
+        return crossedGene;
+    }
+
+    // Crossover help function
+    private static T Crossover<T>(T geneA, T geneB)
+    {
+        Random rand = new Random();
+        if (rand.Next(2) == 1)
+        {
+            return geneA;
+        }
+        return geneB;
+    }
+
+    // Mutation help function
+    private static double MutateDouble(double gene, bool allowNegative)
+    {
+        double mutated = MathUtility.RandomGaussian(gene, gene * STD_DEVIATION_FACTOR);
+        if (!allowNegative)
+        {
+            mutated = Math.Abs(mutated);
+        }
+        // if gene == 0 we will cant mutate, but it's very unlikely to happen.
+        return mutated;
+    }
+
+    // Mutation help function
+    private static bool MutateBool(bool gene)
+    {
+        return !gene;
+    }
+
+
 
 
 
