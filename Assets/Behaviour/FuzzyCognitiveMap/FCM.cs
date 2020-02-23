cc048ff80ac855842d5bc38c74c6d0abb00c3bbc
nj-kore <korejonathan@gmail.com>
2020-02-23 21:50:03 +0100

    Added basic fcm

---
+++ Assets/Behaviour/FuzzyCognitiveMap/FCM.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+
+
+public class FCM
+{
+    double[,] weights;
+    double[] states;
+    int NOFields;
+    int NOActions;
+    System.Random r = new System.Random();
+
+    public enum Field
+    {
+        FoodClose,
+        GoToFood,
+        Idle
+    }
+
+    public enum Action
+    {
+        GoToFood = Field.GoToFood,
+        Idle = Field.Idle
+    }
+
+    public enum Input
+    {
+        FoodClose = Field.FoodClose
+    }
+
+    // This is for fields such as "fear"
+    public enum Intermediates
+    {
+
+    }
+
+    public FCM()
+    {
+        NOFields = Enum.GetNames(typeof(Field)).Length;
+        NOActions = Enum.GetNames(typeof(Action)).Length;
+        weights = new double[NOFields, NOFields];
+        states = new double[NOFields];
+        Debug.developerConsoleVisible = true;
+    }
+
+
+    public void Calculate()
+    {
+        double[] new_states = (double[])states.Clone();
+
+        for(int _from = 0; _from < NOFields; _from++)
+        {
+            for (int _to = 0; _to < NOFields; _to++)
+            {
+                new_states[_to] += weights[_from, _to] * states[_from];
+                new_states[_to] = Mathf.Clamp((float)new_states[_to], 0, 1);
+            }
+        }
+
+        states = new_states;
+    }
+
+    
+    public Action GetAction()
+    {
+        Action a = Action.GoToFood;
+
+        double sum = 0;
+        for (int i = (int)a; i < NOFields; i++)
+        {
+            sum += states[i];
+        }
+
+        if (sum > 0)
+        {
+            for (int i = (int)a; i < NOFields; i++)
+            {
+                double prob = states[i] / sum;
+                double roll = r.NextDouble();
+                if (roll < prob)
+                {
+                    return (Action)i;
+                }
+            }
+        }
+        return Action.Idle;
+    }
+
+
+    public void ImpactState(Field state, double impact)
+    {
+        states[(int)state] += impact;
+    }
+
+    public double[] GetStates()
+    {
+        return states;
+    }
+
+    public void SetWeight(Field _from, Field _to, double weight)
+    {
+        weights[(int)_from, (int)_to] = weight;
+    }
+
+}
---
+++ Assets/Tests/FCMTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FCMTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FCMTest()
+        {
+            FCM fcm = new FCM();
+            fcm.SetWeight(FCM.Field.FoodClose, FCM.Field.GoToFood, 1);
+            fcm.ImpactState(FCM.Field.FoodClose, 1);
+            fcm.ImpactState(FCM.Field.Idle, 0.5);
+
+            fcm.Calculate();
+            foreach(double d in fcm.GetStates()) 
+            {
+                Debug.Log(d);
+            }
+
+            Debug.Log(" ");
+
+            fcm.Calculate();
+
+            foreach (double d in fcm.GetStates())
+            {
+                Debug.Log(d);
+            }
+
+            Debug.Log(" ");
+        }
+
+        [Test]
+        public void DefuzzificationTest()
+        {
+            FCM fcm = new FCM();
+            fcm.SetWeight(FCM.Field.FoodClose, FCM.Field.GoToFood, 1);
+            fcm.ImpactState(FCM.Field.GoToFood, 1);
+            fcm.ImpactState(FCM.Field.Idle, 0.5);
+
+            Dictionary<FCM.Action, int> dict = new Dictionary<FCM.Action, int>();
+            for(int i = 0; i < 1000; i++)
+            {
+                FCM.Action action = fcm.GetAction();
+                if (dict.ContainsKey(action)) {
+                    dict[action] += 1;
+                }
+                else
+                {
+                    dict.Add(action, 1);
+                }
+            }
+
+            Debug.Log(dict.ToString());
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator FCMTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
