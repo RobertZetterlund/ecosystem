7d00ff319f3de2d8f6864f86767434696933387a
nj-kore <korejonathan@gmail.com>
2020-02-23 22:11:08 +0100

    FCMHandler

--- Assets/Behaviour/FuzzyCognitiveMap/FCMHandler.cs
+++ Assets/Behaviour/FuzzyCognitiveMap/FCMHandler.cs
@@ -6,5 +6,18 @@ public class FCMHandler
 
     private FCM fcm;
 
-    public
+    public FCMHandler(FCM fcm)
+    {
+        this.fcm = fcm;
+    }
+
+    public void Calculate()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            fcm.Calculate();
+        } 
+    }
+
+    public 
 }
