524b8805f57d5438881c3a2266c1bf42e9f49ed1
nj-kore <korejonathan@gmail.com>
2020-03-10 10:38:03 +0100

    Created custom controller for game editor that makes it possibler to spawn rabbits and plants individually

--- Assets/Controllers/GameController.cs
+++ Assets/Controllers/GameController.cs
@@ -6,8 +6,7 @@ public class GameController : MonoBehaviour
     private int nPlants = 10;
     private int nRabbits = 5;
 
-
-    void Start()
+    public void SpawnEntities()
     {
         // spawn first rabbits
         for (int i = 0; i < nRabbits; i++)
@@ -17,11 +16,18 @@ public class GameController : MonoBehaviour
         // spawn first plants
         for (int i = 0; i < nPlants; i++)
         {
-            OrganismFactory.CreatePlant(1,NavMeshUtil.GetRandomLocation());
+            OrganismFactory.CreatePlant(1, NavMeshUtil.GetRandomLocation());
         }
+    }
 
+    public void SpawnRabbit()
+    {
+        OrganismFactory.CreateAnimal(Species.Rabbit, 1, 1, 3, 0.1, 0.02, 4, new Vector3(0,0,0));
+    }
 
-
+    public void SpawnPlant()
+    {
+        OrganismFactory.CreatePlant(1, new Vector3(0, 0, 10));
     }
 
 
