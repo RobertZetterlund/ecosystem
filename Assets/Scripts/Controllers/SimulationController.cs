d48e3e9ca3325ab44045b37fa889f8922b835dbb
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2020-04-03 16:06:24 +0200

    correct arrays in init of animaltraits

--- Assets/Scripts/Controllers/SimulationController.cs
+++ Assets/Scripts/Controllers/SimulationController.cs
@@ -127,12 +127,13 @@ abstract class SimulationController : MonoBehaviour
     private void InitBaseTraits()
     {
         String[] rabbitArr = new String[] {"Rabbit" };
-        String[] foxArr = new String[] { "Rabbit" };
-        String[] plantArr = new String[] { "Rabbit" };
+        String[] foxArr = new String[] { "Fox" };
+        String[] plantArr = new String[] { "Plant" };
+        String[] emptyArr = new string[] { "" };
 
 
         AnimalTraits rabbitTraits = new AnimalTraits(Species.Rabbit, 3, 0, 2, 0.1, 0.2, 3, 20, 30, 25, new RabbitFCMHandler(FCMFactory.RabbitFCM()), plantArr, foxArr, rabbitArr);
-        AnimalTraits foxTraits = new AnimalTraits(Species.Fox, 3, 0, 2, 0.1, 0.2, 3, 20, 30, 25, new FoxFCMHandler(FCMFactory.FoxFCM()), rabbitArr, new[] {""}, foxArr);
+        AnimalTraits foxTraits = new AnimalTraits(Species.Fox, 3, 0, 2, 0.1, 0.2, 3, 20, 30, 25, new FoxFCMHandler(FCMFactory.FoxFCM()), rabbitArr, emptyArr, foxArr);
 
         baseTraits[(int)Species.Rabbit] = rabbitTraits;
         baseTraits[(int)Species.Fox] = foxTraits;
