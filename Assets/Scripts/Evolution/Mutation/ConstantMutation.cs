b4ee8c97f8da1544e618fca9651ac35d57f1dda1
isaksc2 <isaksc@student.chalmers.se>
2020-04-29 10:08:42 +0200

    fix pop 0 + add action ratio

--- Assets/Scripts/Evolution/Mutation/ConstantMutation.cs
+++ Assets/Scripts/Evolution/Mutation/ConstantMutation.cs
@@ -7,7 +7,7 @@ class ConstantMutation : SingletonBase<ConstantMutation>, IMutation
 	//private static double STD_DEVIATION_FACTOR = 0.5;
 	//private static double MUTATION_CHANCE = 0.05;
 	private Random r = new Random();
-	private double mutationSize = 0.05;
+	private double mutationSize = 0.2;
 
 	public RangedDouble Mutate(RangedDouble gene)
 	{
